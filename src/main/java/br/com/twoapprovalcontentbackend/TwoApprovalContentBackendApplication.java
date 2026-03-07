aa62b95632761fab4b46ed3037e36c4db989626b
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2026-03-07 01:53:02 -0300

    feat: timezone

--- src/main/java/br/com/twoapprovalcontentbackend/TwoApprovalContentBackendApplication.java
+++ src/main/java/br/com/twoapprovalcontentbackend/TwoApprovalContentBackendApplication.java
@@ -3,10 +3,13 @@ package br.com.twoapprovalcontentbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TwoApprovalContentBackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
         SpringApplication.run(TwoApprovalContentBackendApplication.class, args);
     }
 
