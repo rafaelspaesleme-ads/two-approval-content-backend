64b9d95458f383d29d4d2eed1da0480adb506c72
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2026-05-02 02:07:31 -0300

    feat: criação do serviço de envio de conteúdo para avaliação.

--- src/main/java/br/com/twoapprovalcontentbackend/infraestructure/configs/securities/fails/CustomAuthenticationSecurityHandler.java
+++ src/main/java/br/com/twoapprovalcontentbackend/infraestructure/configs/securities/fails/CustomAuthenticationSecurityHandler.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 public class CustomAuthenticationSecurityHandler implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
 
 
         Optional.ofNullable(request.getAttribute("error_apiKey"))
